f0177f413ec3cef896352f863b72085442faf36b
senyacherenkov <senyacherenkov@yandex.ru>
2019-06-07 08:55:42 -0700

    done

--- main.cpp
+++ main.cpp
@@ -135,6 +135,16 @@ int removeMax(int* heap, size_t size)
     return max;
 }
 
+int removeByIndex(int* heap, size_t size, size_t index)
+{
+    assert(size >= 1);
+    int element = heap[index];
+    heap[index] = heap[size - 1];
+    size--;
+    drown(heap, index, size);
+    return element;
+}
+
 BOOST_AUTO_TEST_CASE(build_heap_test)
 {    
     int heap[12] = {9, 5, 10, 8, 2, 1, 0, 3, 11, 4, 6, 7};
@@ -182,22 +192,55 @@ BOOST_AUTO_TEST_CASE(sort_heap_test)
 BOOST_AUTO_TEST_CASE(removeMax_test)
 {
     int heap[12] = {9, 5, 10, 8, 2, 1, 0, 3, 11, 4, 6, 7};
-    size_t size = sizeof (heap)/sizeof (int);
-    size_t sizeTrack = size;
-    buildHeap(heap, size);
-    for(size_t i = 0; i <= (size - 1)/2; i++)
+    size_t sizeTrack = sizeof (heap)/sizeof (int);
+    buildHeap(heap, sizeTrack);
+
+    while(sizeTrack > 0)
     {
-        if(i == size - 1)
-            break;
+        //check the heap
+        for(size_t i = 0; i <= (sizeTrack - 1)/2; i++)
+        {
+            if(i == sizeTrack - 1)
+                break;
+
+            BOOST_CHECK_MESSAGE(heap[i] >= heap[getLeftIndex(i)], "left node: " << getLeftIndex(i) << " is not less than parent: " << i);
+
+            if(getRightIndex(i) < sizeTrack)
+                BOOST_CHECK_MESSAGE(heap[i] >= heap[getRightIndex(i)], "right node: " << getRightIndex(i) << " is not less than parent: " << i);
+        }
 
         int element = heap[0];
         BOOST_CHECK_MESSAGE(element == removeMax(heap, sizeTrack--),"number has not highest priority");
-
-        BOOST_CHECK_MESSAGE(heap[i] >= heap[getLeftIndex(i)], "left node: " << getLeftIndex(i) << " is not less than parent: " << i);
-
-        if(getRightIndex(i) < size)
-            BOOST_CHECK_MESSAGE(heap[i] >= heap[getRightIndex(i)], "right node: " << getRightIndex(i) << " is not less than parent: " << i);
     }
+
+    BOOST_TEST_MESSAGE("test is done");
+}
+
+BOOST_AUTO_TEST_CASE(remove_by_index_test)
+{
+    int heap[12] = {9, 5, 10, 8, 2, 1, 0, 3, 11, 4, 6, 7};
+    size_t sizeTrack = sizeof (heap)/sizeof (int);
+    buildHeap(heap, sizeTrack);
+
+    while(sizeTrack > 0)
+    {
+        //check the heap
+        for(size_t i = 0; i <= (sizeTrack - 1)/2; i++)
+        {
+            if(i == sizeTrack - 1)
+                break;
+
+            BOOST_CHECK_MESSAGE(heap[i] >= heap[getLeftIndex(i)], "left node: " << getLeftIndex(i) << " is not less than parent: " << i);
+
+            if(getRightIndex(i) < sizeTrack)
+                BOOST_CHECK_MESSAGE(heap[i] >= heap[getRightIndex(i)], "right node: " << getRightIndex(i) << " is not less than parent: " << i);
+        }
+
+        int element = heap[sizeTrack == 1? 0: 1];
+        BOOST_CHECK_MESSAGE(element == removeByIndex(heap, sizeTrack, sizeTrack == 1? 0: 1),"number has not highest priority");
+        sizeTrack--;
+    }
+
     BOOST_TEST_MESSAGE("test is done");
 }
 BOOST_AUTO_TEST_SUITE_END()
